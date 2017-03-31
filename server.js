ff1943267023af6b0a00e4ef44b7bb80e03e18ee
aaron ward <aaron ward>
2017-03-31 14:34:35 +0100

    initial commit for dublin bus API

---
+++ server.js
@@ -0,0 +1,85 @@
+//Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+
+//Extract data from HTTP requests
+var app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+//Set the port
+var port = process.env.port || 3030;
+var router = express.Router();  
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    console.log('Something is happening.');
+    next(); 
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'This is the index page for the Bus API...' });   
+});
+
+//Start Server
+app.listen(port);
+console.log('Running on port : ' + port);
+
+/**
+ * Api to make request to bus RTPI API
+ */
+router.route('/bus').get(function(req, res) {
+
+        var options = {
+            url: 'https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=4747&format=json',
+            method : 'GET',
+            strictSSL: false
+        }; 
+
+        var message;
+        var busNumber = 239;
+        var all = false;
+
+        request(options, function(error, response, body) {
+        if(error){
+            console.log(error);
+        }
+        else{
+            body = JSON.parse(body);
+            //numberofresults will return as 0 if it past half 11
+            if(body.numberofresults === 0){
+                message = "It's too late for busses mate";
+            }
+            else{
+                var resultCount = 0;
+                //Display all the bus routes and due times available
+                for( var i in body.results){
+                    if(body.results[i].route == busNumber || all == true){
+                        //If the bus is due now, dont display "due in due minutes"
+                        if(body.results[i].duetime === "Due"){
+                            message += body.results[i].route + " to " + body.results[i].destination + " due now\n";
+                        }
+                        //Stop 1 minute appearing as "1 minutes"
+                        else if(body.results[i].duetime === "1"){
+                            message += body.results[i].route + " to " + body.results[i].destination + " due in " + body.results[i].duetime 
+                            + " minute\n";
+                        }
+                        else{
+                            message += body.results[i].route + " to " + body.results[i].destination + " due in " + body.results[i].duetime 
+                            + " minutes\n";
+                        }
+                        resultCount++;
+                    }
+                }
+                //Check if there is not times available
+                if(resultCount === 0){
+                    message = "There is no times available for " + busNumber + " 😕";
+                }
+            }
+            //Send the result back to the requester
+            res.send({ message });
+        }
+    });
+});
